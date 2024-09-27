aa055d088ecf73e3c6e0cd2c2b44edc76b0e1027
EC2 Default User <ec2-user@ip-172-31-23-118.ap-northeast-1.compute.internal>
2024-09-27 01:24:17 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create,:destroy]
   end
 
+  get '/search', to: 'searches#search'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
