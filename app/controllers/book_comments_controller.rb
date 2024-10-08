ade1babd540783ff2b184059089705c3fa7b6f79
EC2 Default User <ec2-user@ip-172-31-14-26.ap-northeast-1.compute.internal>
2024-10-08 13:24:55 +0000

    Initial Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,7 +8,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    @comment = BookComment.find(params[:id])
+    @comment = BookComment.find_by(id: params[:id],book_id: params[:book_id])
     @comment.destroy
   end
 
